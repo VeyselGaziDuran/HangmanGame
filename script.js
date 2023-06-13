62fc7d48ee303aa87ba136dfe667dbbfa4889caf
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-06-13 23:21:22 +0300

    wrong letters were shown and as a result the man was hanged :)

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container')
 const message_el = document.getElementById('success-message')
+const wrongLetters_el = document.getElementById('wrong-letters') 
+const items = document.querySelectorAll('.item')
 
-const correctLetters =['a', 'b', 'c'];
+const correctLetters =[];
 const wrongLetters = [];
+
 const selectedWord = getRandomWord();
 
 function getRandomWord(){
@@ -27,8 +30,23 @@ function displayWord(){
         popup.style.display = 'flex';
         message_el.innerText = 'Congratulations You Won'
     }
-    
+}
 
+function updateWrongLetters() {  
+    wrongLetters_el.innerHTML = `
+        ${wrongLetters.length>0? ' <h3>Incorrect Letters</h3>': '' }  
+        ${wrongLetters.map(letter=> `<span>${letter}</span>`)}   
+    `;
+
+    items.forEach((item,index) => {
+        const errorCount = wrongLetters.length;
+
+        if(index<errorCount){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    })
 }
 
 
@@ -47,8 +65,8 @@ window.addEventListener('keydown', function(e){
         }else{
             if(!wrongLetters.includes(letter)){
                 wrongLetters.push(letter);
-                // hatalı harfleri güncelle
-            }
+                updateWrongLetters();
+            };
         }
     }
 });
