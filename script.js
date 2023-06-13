e26f01cd3885aa96c791e26a44fb8116cb4fe29f
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-06-13 21:27:01 +0300

    random words shown to user

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const word_el = document.getElementById('word')
+const word_el = document.getElementById('word');
+
+const correctLetters =['j','a','v','s','g'];
+const wrongLetters = [];
 
 function getRandomWord(){
     const words = ["Python", "JavaScript", "PHP", "Java", "Ruby", "Swift", "C#", "Go", "Kotlin", "TypeScript", "Dart", "Rust", "Lua", "Perl", "Scala", "R", "MATLAB", "Erlang"];
     return words[Math.floor(Math.random() * words.length)];
 }
-
-console.log(getRandomWord());
 
 function displayWord(){
     const selectedWord = getRandomWord();
@@ -13,10 +14,17 @@ function displayWord(){
     word_el.innerHTML = `
         ${selectedWord.split('').map(letter => `
             <div class="letter">
-                ${letter}
+                ${correctLetters.includes(letter) ? letter: ''}
             </div>
         `).join('')}
     `;
+
+    const w = word_el.innerText.replace(/\n/g,'');
+    if(w === selectedWord){
+        console.log('you won');
+    }
+    
+
 }
 
 displayWord();
